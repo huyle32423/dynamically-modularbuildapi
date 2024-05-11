b965b88556853edcb8aac058913e83d1933cc17b
huyle32423 <samielite9nam1311@outlook.com>
2024-05-11 09:33:27 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function longestValidParentheses(s) {
-  if (s.length === 0) return 0;
-  let max = 0;
-  const dp = new Array(s.length).fill(0);
-  for (let i = 1; i < s.length; i++) {
-    if (s[i] === ")") {
-      if (s[i - 1] === "(") dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-      else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] === "(")
-        dp[i] =
-          dp[i - 1] + (i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
-      max = Math.max(max, dp[i]);
-    }
-  }
-  return max;
+function getMin(arr) {
+  return Math.min(...arr);
 }
