d810055ec030266d2bfaece285a4dfe61a8b1f75
huyle32423 <samielite9nam1311@outlook.com>
2024-05-15 09:28:49 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const gcd = (a, b) => {
-  if (b === 0) {
-    return a;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
   }
-  return gcd(b, a % b);
-};
+  return i === s.length;
+}
